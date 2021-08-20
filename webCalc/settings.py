0ad31857f7a72e1b5e31bee7a706d3257dd5abbf
Ife Olulesi <53560106+IfeOlulesi@users.noreply.github.com>
2021-08-20 12:18:52 +0100

    .

--- webCalc/settings.py
+++ webCalc/settings.py
@@ -42,8 +42,8 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleWare',
     'django.contrib.sessions.middleware.SessionMiddleware',
-    'whitenoise.middleware.WhiteNoiseMiddleWare'
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
